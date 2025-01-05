29764f87e90ce4b27a6f0f686c6f8c8aa0bbd6f6
KAMDEM Ivann <kapoivha@gmail.com>
2025-01-05 02:59:32 +0100

    matplotlib tests

---
+++ Formation-Saint_Cirgue/python-ml/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+f = lambda x, y: np.sin(x) + np.cos(x+y)
+
+X = np.linspace(0, 5, 100)
+Y = np.linspace(0, 5, 100)
+X, Y = np.meshgrid(X, Y)
+Z = f(X, Y)
+ax = plt.axes(projection='3d')
+ax.plot_surface(X, Y, Z, cmap='plasma')
+plt.show()
